2585f2f7c737e3722aaaaaaf8a94f3056408e6d7
el_qemary <abdessamadelqemary13370101@gmail.com>
2026-02-16 17:58:07 +0000

    Add files via upload

---
+++ identifiers Name convention and rules.js
@@ -0,0 +1,13 @@
+/*
+ ..identifiers
+ ..Name convention And Rules => Rules: can use letters, digits, $
+  and _, cannot start with a number, case-sensitive,
+   and cannot use reserved words
+ ..Reserved Words =>Reserved Words are keywords that
+  have a special meaning in JavaScript.
+You cannot use them as variable names,
+function names, or identifiers.
+
+*/
+var user = "elqemary";
+console.log(user);
