d086a84b0984a5e07760640d9b217f2739da17d3
el_qemary <abdessamadelqemary13370101@gmail.com>
2026-02-16 17:44:43 +0000

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,16 @@
+/*
+ ..variables Intoduction =>    .
+ ..what is variable ? => a container used to store data that can be used and changed later
+ ..why we use variables ? => to store data
+ ..Declare A variable and Use ?  
+ ..Syntax (keyword variable name = variable value) =>
+ ..variable without var  =>  you can declare a variable without var, let, or const, but it’s not recommended.
+ .. ID and global variable => variable that can be accessed anywhere in your code
+*/
+
+   var user = "abdessamad";
+   var age = 25;
+   console.log(user);
+   console.log(age)
+
+
