86e2016ff3cf90c21582010956534a8c0869fea2
el_qemary <abdessamadelqemary13370101@gmail.com>
2026-02-16 17:20:42 +0000

    Add files via upload

---
+++ dataTypes.js
@@ -0,0 +1,23 @@
+/*
+..datatypes
+...string
+..Number and Integr
+..Array => Object
+..Object 
+..Bolean => true and false 
+.. undefined => a variable that is declared but has no value assigned yet
+..null =>  a variable that is intentionally set to have no value
+*/
+
+
+
+console.log("abdessamadelqemary");
+console.log(typeof ("abdessamadelqemary"));
+console.log(2003)
+console.log(typeof 2003);
+console.log(typeof ["mohammed","aya", "omar"]);
+console.log(typeof [1, 2, 3]);
+console.log(typeof { name: "aya", age: 18, country: "Mo"});
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
