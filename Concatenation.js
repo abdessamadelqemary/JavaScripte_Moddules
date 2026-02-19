de389f4673fb215cda52a16f1921058a02c43573
abdessamad el_qemary <abdessamadelqemary13370101@gmail.com>
2026-02-19 11:31:49 +0000

    Add files via upload

---
+++ Concatenation.js
@@ -0,0 +1,9 @@
+/*
+..Concatenation =>  Concatenation is just a fancy word for "gluing things together."
+..=> You use the plus sign (+) to stick text together.
+*/
+
+let a = "We Love ";
+let b = "Javascripte";
+document.write(a + " " + b);
+console.log(a, b)
