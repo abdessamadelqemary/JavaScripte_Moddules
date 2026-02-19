6af7b3a40c9707e0fb1b26888a90a546594cd536
abdessamad el_qemary <abdessamadelqemary13370101@gmail.com>
2026-02-19 13:19:14 +0000

    Add files via upload

---
+++ Type_Coercion.js
@@ -0,0 +1,23 @@
+/* 
+..Type Coercion (type Casting)
+.. + 
+.. -
+.. ""
+.. -2
+.. false true
+
+*/
+
+let a = "10";
+let b = 20;
+let c = true;
+
+console.log(a + b);
+
+console.log("" - 2);
+console.log(+"");
+
+console.log(false + true);
+
+
+
