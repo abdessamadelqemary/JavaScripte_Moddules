f29f0f7049b7f34e3792cfb176168effe71b8e26
abdessamad el_qemary <abdessamadelqemary13370101@gmail.com>
2026-02-19 13:07:40 +0000

    Add files via upload

---
+++ Operators.js
@@ -0,0 +1,39 @@
+/*
+Arithmetic Operators
++ => addition
+- => subtraction
+* => multiplication
+/ => division
+** => exponentiation
+% => modulus (division remainder)
+++ => increment => num++
+-- => dectement => mum --
+
+*/
+
+//addition
+console.log(10 + 20); // 30
+console.log(10 + "elqemary")
+
+//subtraction
+console.log(20 - 10); //10
+console.log(20 + "ossama"); //NAN 
+console.log(typeof NaN); //this is number
+
+// multiplication
+console.log(20 * 10); //200
+console.log(20 * -10); // -200
+
+//division
+console.log(20 / 10);// 2
+console.log(20 / -10); // -2
+
+//exponentiation os
+console.log(4 ** 2); // 16
+console.log(8 ** 2); // 64
+
+//remainder
+console.log(11 % 2) // 1 al ba9i
+
+
+
