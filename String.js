2bab6aca2fe7b4b4f66ee92dff5b577159a74b7f
abdessamad el_qemary <abdessamadelqemary13370101@gmail.com>
2026-02-19 11:22:18 +0000

    Add files via upload

---
+++ String.js
@@ -0,0 +1,10 @@
+/*
+..string syntax + character Escape Sequences
+.. escape + line continue
+..\n
+character Escape Sequences => The backslash tells the computer: "Don't treat the next quote as the end of the text; just print it as a symbol."
+*/
+console.log('abdessamad "elqemary"');
+console.log("abdessamad 'elqemary'");
+console.log("abdessamad \"elqemary\"");
+console.log('abdessamad  \'elqemary\'');
