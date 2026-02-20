7e9f7921f19a42d74399c212b8290eca5463da74
abdessamad el_qemary <abdessamadelqemary13370101@gmail.com>
2026-02-20 11:00:01 +0000

    Add files via upload

---
+++ Challenges2.js
@@ -0,0 +1,41 @@
+/* 
+..challenges 1
+*/
+
+let a = 10;
+let b = "20";
+let c = 80;
+
+console.log(++a + +b++ + +c++ - +a++); // = 100
+console.log(++a + -b + +c++ - -a++ + +a); // = 82
+console.log(--c + +b + --a * +b++ - +b * a + --a +true);// =102
+
+/* 
+..[++a] [+]
+..[++a]
+.. value
+.. explain
+..[+]
+..explain
+
+*/
+
+/* 
+...challenge 2
+*/
+
+let d = "-100";
+let e = "20";
+let f = 30;
+let g = true;
+
+//only use variables value
+//do not use variabels twis 
+
+console.log(); // 2000
+console.log(); //173
+
+
+
+// Note: Rely on your own external observations in the following exercises; there may be errors.
+
