adde3c12ad4e3c9a614a191210905194ebb45017
abdessamad el_qemary <abdessamadelqemary13370101@gmail.com>
2026-02-20 10:23:54 +0000

    Add files via upload

---
+++ assignment_operators.js
@@ -0,0 +1,19 @@
+/* 
+
+ Assignment Operators => 
+
+*/
+
+let a = 10;
+a = 20; //The second variable will print the first one, which will be its negation.
+a = a + 20 // this is correct 
+a = a + 70;
+
+a += 100; // this is a = a + 100; => and this is  Assignment Operators
+a -= 50; //  a = a - 50;
+a /= 50 // a = a / 50;
+console.log(a)
+
+
+
+
