d109f013fbda4a3abaffd419300ae3aa188eb2f9
abdessamad el_qemary <abdessamadelqemary13370101@gmail.com>
2026-02-20 11:14:00 +0000

    Add files via upload

---
+++ Number.js
@@ -0,0 +1,36 @@
+/* 
+..Number 
+..double precision
+..syntactic sugar
+..e 
+..**
+..whit decimal
+..number + bigInt
+..number min value 
+..number max value 
+*/
+
+console.log(1000000);
+console.log(1_000_000);
+console.log(1e6) // 1000000
+console.log(10 ** 6); //10 os 6
+console.log(1000000.00); // 1000000
+
+console.log(Number.MAX_SAFE_INTEGER); //9007199254740991
+console.log(Number.MIN_SAFE_INTEGER); //-9007199254740991
+console.log(Number.MAX_VALUE) //1.7976931348623157e+308  
+console.log(Number.MIN_VALUE) // 5e-324 => 50324
+
+
+
+
+
+
+
+
+
+
+
+
+
+
