865e477427c8eadb85074690f8011a2ae711c926
abdessamad el_qemary <abdessamadelqemary13370101@gmail.com>
2026-02-20 11:55:01 +0000

    Add files via upload

---
+++ Math_Object.js
@@ -0,0 +1,37 @@
+/*
+..Math object
+..round() =>// Rounds x to the nearest integer
+..ceil() =>// Rounds x up to the next largest integer
+..floor() =>// Rounds x down to the next smallest integer
+..min() =>// Returns the smallest value among the given numbers
+..max() =>// Returns the largest value among the given numbers
+..pow() => // Returns base raised to the power of exponent
+..random() => // Returns a random decimal number between 0 and 1
+..trunc() - [ES6] => // Removes the decimal part of x and returns only the integer
+
+*/
+
+console.log(Math.round(99.2))//99.00
+console.lpg(Math.round(99.5)); // 100
+
+console.log(Math.ceil(99.2))// 100
+console.log(Math.ceil(99.8))//99.00
+
+console.log(Math.min(10 , 50 , 80 , 12 , 79)); //10
+console.log(Math.max(10 , 80 , 50 , 150 , 300)); //300
+
+console.log(Math.pow(4,2));
+
+console.log(Math.random());
+
+
+
+
+
+
+
+
+
+
+
+
